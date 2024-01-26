ac7d879d2c4b246b6418bdbb21b01be398cac5ea
Xavier Pashley <xavipashley@gmail.com>
2024-01-26 15:36:06 +0000

    enhancement: added fav

--- src/componants/picCard/PicCard.js
+++ src/componants/picCard/PicCard.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./PicCard.css"
-
+import { addUserFav } from "../../utils/fetch";
 const PicCard = ({ pic }) => {
-    const handleClick = (e, pic) => {
+    const handleClick = async (e, pic, setUserFav) => {
         console.log("Hello From PicCard", pic.id)
-    }
+        e.preventDefault();
+        console.log("id_photo: ", favPic.id);
+        console.log("url: ", favPic.url);
+        const data = await addUserFav(favPic.id, favPic.url);
+        console.log(data);
+        setUserFav(data.img);
+    };
 
         return (
-            <div className="PicCard" onClick={(e) => handleClick (e, pic)}>
+
+            <div className="PicCard" onClick={(e) => handleClick (e, pic,)}>
                 <div className="PicCard-Img-Container">
                     <img className="PicCard-Img" src={pic.pics.small}/>
                 </div>
